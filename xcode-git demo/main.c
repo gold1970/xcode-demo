ea14cddce32ea6a9230fb46f51c2392c368ffa43
gold1970 <jisus4u@naver.com>
2022-02-26 14:15:28 +0900

    Initial Commit

---
+++ xcode-git demo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  xcode-git demo
+//
+//  Created by dmg on 2022/02/26.
+//  Copyright © 2022 dmg. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
